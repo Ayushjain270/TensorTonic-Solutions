c25b60b543872aea81d289d77cdd9fb46b503e5c
Ayushjain270 <aj1183693@gmail.com>
2026-03-18 11:49:59 +0530

    Add solution for linear-regression-closed-form

---
+++ linear-regression-closed-form/linear-regression-closed-form.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def linear_regression_closed_form(X, y):
+    """
+    Compute the optimal weight vector using the normal equation.
+    """
+    rows = len(X)
+    cols = len(X[0])
+    t = []
+    for i in range(cols):
+        new =[]
+        for j in range(rows):
+            new.append(X[j][i])
+        t.append(new)
+        
+    result = np.linalg.inv(np.dot(t, X )).dot(t).dot(y)
+    return result
