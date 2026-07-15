1cd1002610f61747a4b8490d402e8cdd26bc6c5b
Ayushjain270 <aj1183693@gmail.com>
2026-07-15 18:35:49 +0530

    Solve lstm-cell

---
+++ lstm/lstm-cell/lstm-cell.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
+
+def lstm_cell(x_t: np.ndarray, h_prev: np.ndarray, C_prev: np.ndarray,
+              W_f: np.ndarray, W_i: np.ndarray, W_c: np.ndarray, W_o: np.ndarray,
+              b_f: np.ndarray, b_i: np.ndarray, b_c: np.ndarray, b_o: np.ndarray) -> tuple:
+    """Complete LSTM cell forward pass."""
+    def sigmoid(x):
+        return 1/(1+ np.exp(-x))
+
+    combine = np.concatenate((h_prev , x_t), axis = -1)
+    z_f = combine @ W_f.T + b_f
+    f_t = sigmoid(z_f)
+
+    i_t= sigmoid(combine @W_i.T + b_i)
+    c_tilde = np.tanh(combine@ W_c.T + b_c)
+    o_t = sigmoid(combine @ W_o.T + b_o)
+    
+    c_t = f_t*C_prev + i_t*c_tilde
+    h_t = o_t*np.tanh(c_t)
+
+    return (h_t , c_t)
+    
