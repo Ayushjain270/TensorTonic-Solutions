2e40ca9d67462eb88b6f2f1721f36ecd2025c3b3
Ayushjain270 <aj1183693@gmail.com>
2026-07-16 14:26:22 +0530

    Solve gru-reset-gate

---
+++ gru/gru-reset-gate/gru-reset-gate.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
+
+def reset_gate(h_prev: np.ndarray, x_t: np.ndarray,
+               W_r: np.ndarray, b_r: np.ndarray) -> np.ndarray:
+    """
+    Compute reset gate: r_t = sigmoid(W_r @ [h, x] + b_r)
+    """
+    def sigmoid(x):
+        return 1 /(1+np.exp(-x))
+    combine = np.concatenate([h_prev , x_t] , axis =-1)
+    z = combine @ W_r.T + b_r
+    r_t = sigmoid(z)
+    return r_t
